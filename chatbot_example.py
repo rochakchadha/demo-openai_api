f687ee5a76de5a16f67873b1b3d69beb72b4e59d
rochakchadha <83778749+rochakchadha@users.noreply.github.com>
2023-11-02 01:03:03 -0400

    changed colors to red for the number of tokens generated, time and token speed, for better readability

--- chatbot_example.py
+++ chatbot_example.py
@@ -1,5 +1,8 @@
 import openai
 import os
+import time
+from colorama import Fore, Style
+
 # Initialize the OpenAI API with your key
 openai.api_key = os.getenv("OPENAI_API_KEY")
 conversation_history = []
@@ -10,12 +13,24 @@ def chat_with_gpt3(prompt):
     """
     global conversation_history
     conversation_history.append({"role": "user", "content": prompt})
-
+    # calculate the response time from GPT-3
+    start_time = time.time()
     response = openai.ChatCompletion.create(
       model="gpt-3.5-turbo",
       max_tokens=100,  # You can adjust the number of tokens (words) you want in the response
       messages=conversation_history,
     )
+    end_time = time.time()
+    print(Fore.RED) # All prints after this in the function will be red
+
+    # print the response time
+    print(f"Response time: {end_time - start_time} seconds")
+    # get number of generated tokens
+    generated_tokens = response['usage']['completion_tokens']    
+    print(f"Generated tokens: {generated_tokens}")
+    # calculate generated tokens per second
+    print(f"Generated tokens per second: {generated_tokens / (end_time - start_time)} Tks/s")
+
     # Extract the assistant's message from the response   
     assistant_message = response.choices[0].message['content']
 
@@ -24,8 +39,8 @@ def chat_with_gpt3(prompt):
 
       # Print the number of tokens used
     tokens_used = response['usage']['total_tokens']
-    print(f"Tokens used: {tokens_used}")
-
+    print(f"Total Tokens used: {tokens_used}")
+    print(Style.RESET_ALL) # Reset the colorama style to normal White
     return assistant_message
 
 def main():
@@ -34,6 +49,7 @@ def main():
         user_input = input("You: ")
         if user_input.lower() == 'exit':
             break
+        
         response = chat_with_gpt3(user_input)
         
         print(f"Assistant: {response}")
